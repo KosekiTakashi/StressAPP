13be07e0848f740be3aed53b52ad79d96b1ee171
KosekiTakashi <takashi111398@gmail.com>
2019-10-05 01:23:53 +0900

    TabBar, TableViewの大まかな設定
    初期設定

---
+++ Swift5_StressAPP/Controller/ListDetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ListDetailViewController.swift
+//  Swift5_StressAPP
+//
+//  Created by 小関隆司 on 2019/10/04.
+//  Copyright © 2019 kosekitakashi. All rights reserved.
+//
+
+import UIKit
+
+class ListDetailViewController: UIViewController {
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    var name : String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nameLabel.text = name
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Swift5_StressAPP/Controller/ListViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ListViewController.swift
+//  Swift5_StressAPP
+//
+//  Created by 小関隆司 on 2019/10/04.
+//  Copyright © 2019 kosekitakashi. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    var test = ["a","b","c"]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+       
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tableView.reloadData()
+    }
+    
+    //セクションの数
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+
+    }
+    
+    
+    //セルの数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return test.count
+    }
+    
+    //cellの設定
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = test[indexPath.row]
+        
+        return cell
+    }
+    
+    //セルの高さ
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return view.frame.size.height/5
+    }
+    
+    
+    //セルをタッチで画面遷移（ListDetailViewController）
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let nextVC = storyboard?.instantiateViewController(identifier: "next") as! ListDetailViewController
+        
+        nextVC.name = test[indexPath.row]
+        
+        navigationController?.pushViewController(nextVC, animated: true)
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
